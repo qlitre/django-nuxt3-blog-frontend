331d5b5d95d84b14c53d75150678a1a2f8eb31ad
qlitre <kuri0403@gmail.com>
2022-05-22 22:46:29 +0900

    ヘッダーの画像設定をなくした

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar app color="teal-darken-4" image="https://picsum.photos/1920/1080?random">
+  <v-app-bar app color="teal-darken-4">
     <template v-slot:image>
       <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
     </template>
